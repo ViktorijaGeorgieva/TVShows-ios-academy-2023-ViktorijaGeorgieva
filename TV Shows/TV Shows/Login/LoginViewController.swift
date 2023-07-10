09f369f5f5013395fb8f693afeec2a810ee83a1a
Viktorija Georgieva <viktorijageorgieva@gmail.com>
2023-07-10 19:16:07 +0200

    Solve the first extra assignment

--- TV Shows/TV Shows/Login/LoginViewController.swift
+++ TV Shows/TV Shows/Login/LoginViewController.swift
@@ -9,17 +9,31 @@ import UIKit
 
 class LoginViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     var numberOfTaps: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //Do any additional setup after loading the view.
+        view.backgroundColor = .systemMint
+        button.backgroundColor = .yellow
+        button.layer.cornerRadius = 15
+        label.font = UIFont.systemFont(ofSize: 30)
+        activityIndicator.startAnimating()
         
     }
     
     @IBAction func buttonActionHandler(_ sender: Any) {
         numberOfTaps += 1
         label.text = String(numberOfTaps)
+        if activityIndicator.isAnimating {
+            activityIndicator.stopAnimating()
+            button.setTitle("Start", for:  .normal)
+        } else {
+            activityIndicator.startAnimating()
+            button.setTitle("Stop", for: .normal)
+        }
     }
 }
