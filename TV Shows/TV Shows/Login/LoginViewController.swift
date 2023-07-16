657730d77f78a4fa8f66a287eaedb5da03ab514a
Viktorija Georgieva <viktorijageorgieva@gmail.com>
2023-07-16 20:56:26 +0200

    Layouting login screen

--- TV Shows/TV Shows/Login/LoginViewController.swift
+++ TV Shows/TV Shows/Login/LoginViewController.swift
@@ -6,42 +6,9 @@
 //
 
 import UIKit
-import MBProgressHUD
 
 final class LoginViewController: UIViewController {
-    @IBOutlet private weak var numberOfTapsLabel: UILabel!
-    @IBOutlet private weak var incrementButton: UIButton!
-    @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
-    
-    private var numberOfTaps: Int = 0
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        MBProgressHUD.showAdded(to: view, animated: true)
-        setUpUI()
-        animateSpinner()
-    }
-    
-    @IBAction private func incrementButtonPressed() {
-        numberOfTaps += 1
-        numberOfTapsLabel.text = String(numberOfTaps)
-        animateSpinner()
-    }
-    
-    private func setUpUI() {
-        view.backgroundColor = .systemMint
-        incrementButton.backgroundColor = .yellow
-        incrementButton.layer.cornerRadius = 15
-        numberOfTapsLabel.font = UIFont.systemFont(ofSize: 30)
-    }
-    
-    private func animateSpinner() {
-        if activityIndicator.isAnimating {
-            activityIndicator.stopAnimating()
-            incrementButton.setTitle("Start", for:  .normal)
-        } else {
-            activityIndicator.startAnimating()
-            incrementButton.setTitle("Stop", for: .normal)
-        }
     }
 }
