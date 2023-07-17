2bf08d36be97fb71f8f0aae8bedf32539162f6c0
Viktorija Georgieva <viktorijageorgieva@gmail.com>
2023-07-17 20:08:44 +0200

    Fixed errors in the second home assignment

--- TV Shows/TV Shows/Login/LoginViewController.swift
+++ TV Shows/TV Shows/Login/LoginViewController.swift
@@ -8,7 +8,37 @@
 import UIKit
 
 final class LoginViewController: UIViewController {
+    @IBOutlet private weak var rememberMeCheckboxButton: UIButton!
+    @IBOutlet private weak var showPasswordButton: UIButton!
+    
+    @IBOutlet private weak var passwordTextField: UITextField!
+    private var rememberMeIsSelected: Bool = false
+    private var showPasswordIsSelected: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    
+    @IBAction private func rememberMeCheckboxStateChanged(_ sender: UIButton) {
+        if rememberMeIsSelected {
+            rememberMeCheckboxButton.setImage(UIImage(named: "ic-checkbox-selected.pdf"), for: UIControl.State.normal)
+            rememberMeIsSelected = false
+        } else {
+            rememberMeCheckboxButton.setImage(UIImage(named: "ic-checkbox-unselected.pdf"), for: UIControl.State.normal)
+            rememberMeIsSelected = true
+        }
+    }
+    
+    @IBAction private func showPasswordStateChanged(_ sender: UIButton) {
+        if showPasswordIsSelected {
+            passwordTextField.text = "1nf1num@/10st34m"
+            showPasswordIsSelected = false
+            showPasswordButton.setImage(UIImage(named: "ic-invisible.pdf"), for: UIControl.State.normal)
+        } else {
+            passwordTextField.text = "• • • • • • • • • • • • "
+            showPasswordIsSelected = true
+            showPasswordButton.setImage(UIImage(named: "ic-visible.pdf"), for: UIControl.State.normal)
+        }
     }
 }
